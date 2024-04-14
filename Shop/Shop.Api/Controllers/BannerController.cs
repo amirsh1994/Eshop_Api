b180e1e37cc2f9df5f5cbf7024fc0d14391c9bdd
Amir Sharabi <shahrabi527@gmail.com>
2024-04-14 10:24:25 -0700

    BannerController Has Been Completed

---
+++ Shop/Shop.Api/Controllers/BannerController.cs
@@ -0,0 +1,53 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.SiteEntities.Banners.Create;
+using Shop.Application.SiteEntities.Banners.Edit;
+using Shop.Presentation.Facade.SiteEntiries.Banner;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Api.Controllers;
+
+
+public class BannerController : ApiController
+{
+
+    private readonly IBannerFacade _bannerFacade;
+
+    public BannerController(IBannerFacade bannerFacade)
+    {
+        _bannerFacade = bannerFacade;
+    }
+
+    [HttpGet]
+    public async Task<ApiResult<List<BannerDto>>> GetBanners()
+    {
+        var result = await _bannerFacade.GetBanners();
+        return QueryResult(result);
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<ApiResult<BannerDto?>> GetBannerById(long id)
+    {
+        var result = await _bannerFacade.GetBannerById(id);
+        return QueryResult(result);
+    }
+
+
+    [HttpPost]
+    public async Task<ApiResult> CreateBanner(CreateBannerCommand command)
+    {
+        var result = await _bannerFacade.CreateBanner(command);
+        return CommandResult(result);
+    }
+
+
+    [HttpPut]
+    public async Task<ApiResult> EditBanner(EditBannerCommand command)
+    {
+        var result = await _bannerFacade.EditBanner(command);
+        return CommandResult(result);
+
+    }
+}
+
