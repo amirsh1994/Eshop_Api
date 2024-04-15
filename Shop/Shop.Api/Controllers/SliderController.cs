6aa00acc29b69987a693671ef3bef7acb3ce07c3
Amir Sharabi <shahrabi527@gmail.com>
2024-04-14 21:47:15 -0700

    slider controller completed

---
+++ Shop/Shop.Api/Controllers/SliderController.cs
@@ -0,0 +1,56 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.SiteEntities.Sliders.Create;
+using Shop.Application.SiteEntities.Sliders.Edit;
+using Shop.Presentation.Facade.SiteEntiries.Slider;
+using Shop.Query.SiteEntities.DTOs;
+
+namespace Shop.Api.Controllers;
+
+
+public class SliderController : ApiController
+{
+    private readonly ISliderFacade _sliderFacade;
+
+    public SliderController(ISliderFacade sliderFacade)
+    {
+        _sliderFacade = sliderFacade;
+    }
+
+    [HttpGet]
+    public async Task<ApiResult<List<SliderDto>>> GetSliders()
+    {
+        var result = await _sliderFacade.GetSliders();
+        return QueryResult(result);
+    }
+
+
+
+
+    [HttpGet("{id}")]
+    public async Task<ApiResult<SliderDto?>> GetSlider(long id)
+    {
+        var result = await _sliderFacade.GetSliderById(id);
+        return QueryResult(result);
+    }
+
+
+    [HttpPost]
+    public async Task<ApiResult> CreateSlider(CreateSliderCommand command)
+    {
+        var result = await _sliderFacade.CreateSlider(command);
+        return CommandResult(result);
+
+    }
+
+
+
+    [HttpPut]
+    public async Task<ApiResult> EditSlider(EditSliderCommand command)
+    {
+        var result = await _sliderFacade.EditSlider(command);
+        return CommandResult(result);
+    }
+
+}
+
